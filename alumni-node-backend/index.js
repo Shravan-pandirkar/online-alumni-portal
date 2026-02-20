51b82a409807b52be901b156626dd100f0ddaea2
Shravan-pandirkar <shravanpandirkar39r@gmail.com>
2026-02-20 12:40:41 +0530

    update version

--- alumni-node-backend/index.js
+++ alumni-node-backend/index.js
@@ -11,8 +11,6 @@ const PORT = process.env.PORT || 5000;
 
 // ================== MIDDLEWARE ==================
 
-const cors = require("cors");
-
 app.use(cors({
   origin: true,                // allow all origins safely
   methods: ["GET", "POST"],
