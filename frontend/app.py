da22e5234206958e1dcf75fffc6412eb81c48901
Shravan-pandirkar <shravanpandirkar39r@gmail.com>
2026-04-01 22:06:11 +0530

    new ui

--- frontend/app.py
+++ frontend/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # First page → Login
 @app.route('/')
 def home():
-    return redirect(url_for('alumnichat'))
+    return redirect(url_for('frontpage'))
 
 @app.route('/frontpage')
 def frontpage():
