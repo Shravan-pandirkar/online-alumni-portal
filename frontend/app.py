2e6e69ab4577043b3495d8f9b853c348f1520870
Shravan-pandirkar <shravanpandirkar39r@gmail.com>
2026-04-02 17:47:40 +0530

    Events now see it

--- frontend/app.py
+++ frontend/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # First page → Login
 @app.route('/')
 def home():
-    return redirect(url_for('showevent'))
+    return redirect(url_for('frontpage'))
 
 @app.route('/frontpage')
 def frontpage():
